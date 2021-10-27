e192077939da362ca0954fcb6f28fbde503e8a73
FedericoCaf <fedetheshield09@gmail.com>
2021-10-27 17:19:04 +0200

    ok-da-fixare-testi

--- js/script.js
+++ js/script.js
@@ -1,27 +1,59 @@
 const fotoArray = ['img/01.jpg','img/02.jpg','img/03.jpg','img/04.jpg','img/05.jpg'];
 
-const title = [
-  'Svezia',
-  'Svizzera',
-  'Gran Bretagna',
-  'Germania',
-  'Paradise'
-]
+const title = ['Svezia','Svizzera','Gran Bretagna','Germania', 'Paradise'];
 
 const text = [
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
   'Lorem ipsum',
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-  'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
+  'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+];
 
-const items = document.getElementsByClassName('fc-item');
-const boxMini = document.getElementsByClassName('box-mini');
+const boxLeft = document.querySelector('.box-left');
+const boxRight = document.querySelector('.box-right');
+const textArea = document.querySelector('.text-area')
 let contatore = 0;
 
-console.log(items);
-console.log(items[contatore]);
+for(let i=0; i<fotoArray.length; i++){
+
+  const item = document.createElement('div');
+  const miniItem = document.createElement('div');
+  const titleItem = document.createElement('h4');
+  const newsItem = document.createElement('p');
+
+  item.className = 'fc-item';
+  miniItem.className = 'item-mini';
+  titleItem.className = 'title-item';
+  newsItem.className = 'news-item';
+
+
+  if(i=== contatore){
+    item.classList.add('active');
+    miniItem.classList.add('active');
+    titleItem.classList.add('active');
+    newsItem.classList.add('active');
+  } 
+  item.innerHTML = `<img src="${fotoArray[i]}" alt="">`;
+  miniItem.innerHTML = `<img src="${fotoArray[i]}" alt="">`;
+  titleItem.innerHTML = `${title[i]}`;
+  newsItem.innerHTML = `${text[i]}`;
+
+  console.log(title[i]);
+
+  boxLeft.append(item);
+  boxRight.append(miniItem);
+  textArea.append(titleItem);
+  textArea.append(newsItem);
+}
+
+
+const items = document.getElementsByClassName('fc-item');
+const boxMini = document.getElementsByClassName('item-mini');
+
+
+// console.log(items);
+// console.log(items[contatore]);
 
 const prev = document.querySelector('.fc-prev');
 const next = document.querySelector('.fc-next');
@@ -35,8 +67,8 @@ prev.addEventListener('click', function(){
 
  items[contatore].classList.add('active');
  boxMini[contatore].classList.add('active');
- console.log(items);
- console.log(boxMini);
+//  console.log(items);
+//  console.log(boxMini);
  
 })
 
@@ -49,7 +81,7 @@ next.addEventListener('click', function(){
  
   items[contatore].classList.add('active');
   boxMini[contatore].classList.add('active');
-  console.log(items);
+  // console.log(items);
   
  })
 
